0450c48e6aad150e28e1a53daeb2a8c3b3df1445
Alex Wulff <alex@coniferapps.com>
2021-04-26 14:12:34 -0400

    Added display

---
+++ tests/display1.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+import numpy as np
+import cv2
+import time
+
+def sin2d(x,y):
+    """2-d sine function to plot"""
+    return np.sin(x) + np.cos(y)
+
+def getFrame():
+    changes = np.random.uniform(-1.0, 1.0, size = [1, 4096])
+    global seed
+    global generator
+    seed = seed+changes*0.02
+    seed = (seed-np.min(seed))/(np.max(seed)-np.min(seed))*2-1
+    #image = generator.predict(seed)
+    image = generator.predict(changes)
+    image = image[0, :, :, :]
+    return np.uint8(image*255)
+    
+seed = np.random.uniform(-1.0, 1.0, size = [1, 4096])
+generator_path = "g_out_410"
+generator = tf.keras.models.load_model(generator_path)
+
+while True:
+    # Get a numpy array to display from the simulation
+    npimage=getFrame()
+    npimage = cv2.cvtColor(npimage, cv2.COLOR_BGR2RGB)
+    cv2.imshow('image',npimage)
+    cv2.waitKey(1)
+    time.sleep(0.5)
