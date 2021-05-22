6ce4caa2f524b37cf4ebac959de4d3eba445d96c
Alex Wulff <alex@coniferapps.com>
2021-05-22 17:03:16 +0100

    Added generator

--- utils/convert-model.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-
-model_dir="/Users/alex/funstuff-local/gan-test/gan-tests/display/g_out_410/"
-
-converter = tf.lite.TFLiteConverter.from_saved_model(model_dir)
-tflite_model = converter.convert()
-
-with open('../generator.tflite', 'wb') as f:
-  f.write(tflite_model)
